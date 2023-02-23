868f56d441ed6c302b99c570d79c1c2378217032
MykeMalyar <michaelbodybuilder1999@gmail.com>
2023-02-23 10:36:25 +0200

    Initial commit

---
+++ desktop/src/com/mmm/wars/of/ain/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mmm.wars.of.ain.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mmm.wars.of.ain.WarsOfAinGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WarsOfAinGame(), config);
+	}
+}
